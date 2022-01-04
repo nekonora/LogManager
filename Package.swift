7904fed4fab2993d570993653563fe9fb301de19
Filippo Zaffoni <registration.fi.za@outlook.com>
2022-01-04 18:25:00 +0100

    Update tagrelease.yml

--- Package.swift
+++ Package.swift
@@ -1,28 +1,17 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "LogManager",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "LogManager",
-            targets: ["LogManager"]),
+        .library(name: "LogManager", targets: ["LogManager"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LogManager",
-            dependencies: []),
-        .testTarget(
-            name: "LogManagerTests",
-            dependencies: ["LogManager"]),
+        .target(name: "LogManager", path: "Sources"),
+        .testTarget(name: "LogManagerTests", dependencies: ["LogManager"]),
     ]
 )
